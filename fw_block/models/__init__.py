fa58e7f9d911d12bbf659f2821ddf1a1bf3e781f
Mr0rang3 <mr.0r4ng3@proton.me>
2024-07-10 20:40:02 -0400

    Add protected networks model

--- fw_block/models/__init__.py
+++ fw_block/models/__init__.py
@@ -1,3 +1,4 @@
 from fw_block.models.ip import IpAddress
 from fw_block.models.firewall import Firewall
 from fw_block.models.blocked import Blocked
+from fw_block.models.protected_networks import ProtectedNetworks
