a8f8646e087551c73fd292fb3d12b14bc43c658d
Mr0rang3 <mr.0r4ng3@proton.me>
2024-07-12 09:58:10 -0400

    Add all styles to search page, ip details partial template and minimal changes to index and initialize maps script

--- fw_block/static/initializeMaps.js
+++ fw_block/static/initializeMaps.js
@@ -3,7 +3,9 @@ const mapElements = document.querySelectorAll('.map-div');
 mapElements.forEach(mapElement => {
 
     let zoom_level = 5
-    let cords = [parseFloat(mapElement.dataset.latitude), parseFloat(mapElement.dataset.longitude)]
+    let latitude = mapElement.dataset.latitude
+    let longitude = mapElement.dataset.longitude
+    let cords = [parseFloat(latitude), parseFloat(longitude)]
 
     const map = L.map(mapElement).setView(
         cords,
@@ -12,7 +14,7 @@ mapElements.forEach(mapElement => {
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        attribution: `&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> | <a href="https://www.openstreetmap.org/?mlat=${latitude}&mlon=${longitude}#map=5/${latitude}/${longitude}">\u00A0${latitude}\u00A0\u00A0${longitude}\u00A0</a>`
     }).addTo(map);
 
     L.marker(cords).addTo(map);
